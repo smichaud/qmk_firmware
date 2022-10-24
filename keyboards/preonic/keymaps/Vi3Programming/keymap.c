e52fb7a5f7d2e3673e8e642f796cdd7359e41eeb
Sebastien Michaud <sebastien.michaud.2@gmail.com>
2022-10-24 08:45:52 -0400

    Added discord keybind

--- keyboards/preonic/keymaps/Vi3Programming/keymap.c
+++ keyboards/preonic/keymaps/Vi3Programming/keymap.c
@@ -89,9 +89,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_CAPS, KC_F1,   KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_CAPS, KC_F1,   KC_F2,         KC_F3,   KC_F4,   _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
-  _______, KC_F5,   KC_F6,         KC_F7,   KC_F8,   _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______,
-  _______, KC_F9,   KC_F10,        KC_F11,  KC_F12,  _______, _______, BL_TOGG, BL_DEC,  BL_INC,  _______, _______,
+  KC_CAPS, KC_F1,   KC_F2,         KC_F3,   KC_F4,   KC_P7,   KC_P8, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+  _______, KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_P9,   KC_P0, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______,
+  _______, KC_F9,   KC_F10,        KC_F11,  KC_F12,  KC_APP,  KC_RGUI, BL_TOGG, BL_DEC,  BL_INC,  _______, _______,
   _______, _______, QK_BOOTLOADER, _______, _______, _______, _______, _______, _______, _______, WINDOWS, LINUX
 ),
 
