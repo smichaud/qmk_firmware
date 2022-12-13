9774adfe42ad768b0a6a0d836c91011483cbb004
Sebastien Michaud <sebastien.michaud.2@gmail.com>
2022-12-13 11:17:36 -0500

    Actual right ctrl for vm exit

--- keyboards/planck/keymaps/vi3programming/keymap.c
+++ keyboards/planck/keymaps/vi3programming/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,               KC_Q,    KC_W,     KC_E,          KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,         KC_O,           KC_P,    KC_BSPC,
   MT(MOD_LGUI, KC_ESC), KC_A,    KC_S,     KC_D,          KC_F,    KC_G,    KC_H,    KC_J,    KC_K,         KC_L,           KC_SCLN, MT(MOD_LGUI, KC_ENT),
   KC_LSFT,              KC_Z,    KC_X,     KC_C,          KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,      KC_DOT,         KC_SLSH, KC_RSFT,
-  KC_LCTL,              KC_LALT, KC_LGUI,  OSL(_ACCENTS), LOWER,   RAISE,   RAISE,   KC_SPC,  MO(_SYMBOLS), MO(_PERMANENT), KC_LALT, KC_LCTL
+  KC_LCTL,              KC_LALT, KC_LGUI,  OSL(_ACCENTS), LOWER,   RAISE,   RAISE,   KC_SPC,  MO(_SYMBOLS), MO(_PERMANENT), KC_LALT, KC_RCTL
 ),
 
 // Basically use swap space/raise (use actual space) + windows keys
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,     KC_E,          KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,         KC_O,           KC_P,    KC_BSPC,
   KC_ESC,  KC_A,    KC_S,     KC_D,          KC_F,    KC_G,    KC_H,    KC_J,   KC_K,         KC_L,           KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,     KC_C,          KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,      KC_DOT,         KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_LALT, KC_LGUI,  OSL(_ACCENTS), LOWER,   KC_SPC,  KC_SPC,  RAISE,  MO(_SYMBOLS), MO(_PERMANENT), KC_LALT, KC_LCTL
+  KC_LCTL, KC_LALT, KC_LGUI,  OSL(_ACCENTS), LOWER,   KC_SPC,  KC_SPC,  RAISE,  MO(_SYMBOLS), MO(_PERMANENT), KC_LALT, KC_RCTL
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
